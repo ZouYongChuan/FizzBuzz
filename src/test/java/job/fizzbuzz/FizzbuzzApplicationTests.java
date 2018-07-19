aaacf00f208a2212e23af136fdf74ffbdb52c807
zou <rachel.z.li@effem.com>
2018-07-19 22:39:02 +0800

    update the test

--- src/test/java/job/fizzbuzz/FizzbuzzApplicationTests.java
+++ src/test/java/job/fizzbuzz/FizzbuzzApplicationTests.java
@@ -18,58 +18,88 @@ public class FizzbuzzApplicationTests {
     private final FindFizzBuzzAtStage2Impl findFizzBuzzAtStage2 = new FindFizzBuzzAtStage2Impl();
 
     /***
-     *stage1中的方法输入4，应当返回4
+     *stage1中的方法,输入4，应当返回4
      */
     @Test
     public void Stage1ItShouldReturnTheGivenNumberOf4() {
         assertEquals("4", findFizzBuzzAtStage1.Find(4));
     }
 
+    /***
+     *stage1中的方法,输入6，应当返回Fizz
+     */
     @Test
     public void Stage1ItShouldReturnFizzWhenTheGivenNumberIsAMultipleOf6() {
         assertEquals("Fizz", findFizzBuzzAtStage1.Find(6));
     }
 
+    /***
+     *stage1中的方法,输入10，应当返回Buzz
+     */
     @Test
     public void Stage1ItShouldReturnBuzzWhenTheGivenNumberIsAMultipleOf10() {
         assertEquals("Buzz", findFizzBuzzAtStage1.Find(10));
     }
 
+    /***
+     *stage1中的方法,输入15，应当返回FizzBuzz
+     */
     @Test
     public void Stage1ItShouldReturnFizzBuzzWhenTheGivenNumberIsAMultipleOf3And5() {
         assertEquals("FizzBuzz", findFizzBuzzAtStage1.Find(15));
     }
 
+    /***
+     *stage2中的方法,输入7，应当返回7
+     */
     @Test
     public void Stage2ItShouldReturnTheGivenNumberOf7() {
         assertEquals("7", findFizzBuzzAtStage2.Find(7));
     }
 
+    /***
+     *stage2中的方法,输入9，应当返回Fizz
+     */
     @Test
     public void Stage2ItShouldReturnFizzWhenTheGivenNumberIsAMultipleOf9() {
         assertEquals("Fizz", findFizzBuzzAtStage2.Find(9));
     }
 
+    /***
+     *stage2中的方法,输入20，应当返回Buzz
+     */
     @Test
     public void Stage2ItShouldReturnBuzzWhenTheGivenNumberIsAMultipleOf20() {
         assertEquals("Buzz", findFizzBuzzAtStage2.Find(20));
     }
 
+    /***
+     *stage2中的方法,输入15，应当返回FizzBuzz
+     */
     @Test
     public void Stage2ItShouldReturnFizzBuzzWhenTheGivenNumberIsAMultipleOf3And5() {
         assertEquals("FizzBuzz", findFizzBuzzAtStage2.Find(15));
     }
 
+    /***
+     *stage2中的方法,输入51，应当返回FizzBuzz
+     */
     @Test
     public void Stage2ItShouldReturnFizzBuzzWhenTheGivenNumberIsAMultipleOf3AndHasA5InIt() {
         assertEquals("FizzBuzz", findFizzBuzzAtStage2.Find(51));
     }
 
+    /***
+     *stage2中的方法,输入35，应当返回FizzBuzz
+     */
     @Test
     public void Stage2ItShouldReturnFizzBuzzWhenTheGivenNumberIsAMultipleOf5AndHasA3InIt() {
         assertEquals("FizzBuzz", findFizzBuzzAtStage2.Find(35));
     }
 
+    /***
+     *stage2中的方法,输入53，应当返回FizzBuzz
+     */
     @Test
     public void Stage2ItShouldReturnFizzBuzzWhenTheGivenNumberHasA5InItAndHasA3InIt() {
         assertEquals("FizzBuzz", findFizzBuzzAtStage2.Find(53));
