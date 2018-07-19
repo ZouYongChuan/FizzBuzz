e588df38ac6788583a7d30f52fe652ff27f4d923
zou <rachel.z.li@effem.com>
2018-07-19 22:47:14 +0800

    update a param

--- src/main/java/job/fizzbuzz/ServiceImpl/FizzBuzzImpl.java
+++ src/main/java/job/fizzbuzz/ServiceImpl/FizzBuzzImpl.java
@@ -6,26 +6,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class FizzBuzzImpl {
     public String Show(int stage,int n) {
-        StringBuilder MyStringBuilder = new StringBuilder("Output:<br/>");
+        StringBuilder outPutString = new StringBuilder("Output:<br/>");
         switch (stage) {
             case 1:
                 FindFizzBuzzAtStage1Impl FindResultAtStage1 =new FindFizzBuzzAtStage1Impl();
                 for (int i = 1; i <= n; ++i) {
-                    MyStringBuilder.append(FindResultAtStage1.Find(i) + "</br>");
+                    outPutString.append(FindResultAtStage1.Find(i) + "</br>");
                 }
                 break;
             case 2:
                 FindFizzBuzzAtStage2Impl FindResultAtStage2 =new FindFizzBuzzAtStage2Impl();
                 for (int i = 1; i <= n; ++i) {
-                    MyStringBuilder.append(FindResultAtStage2.Find(i) + "</br>");
+                    outPutString.append(FindResultAtStage2.Find(i) + "</br>");
                 }
                 break;
             default:
                 FindFizzBuzzAtStage1Impl FindResultAtStageDefault =new FindFizzBuzzAtStage1Impl();
                 for (int i = 1; i <= n; ++i) {
-                    MyStringBuilder.append(FindResultAtStageDefault.Find(i) + "</br>");
+                    outPutString.append(FindResultAtStageDefault.Find(i) + "</br>");
                 }
         }
-        return MyStringBuilder.toString();
+        return outPutString.toString();
     }
 }
